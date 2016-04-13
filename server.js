05e3171569c2e26a25c847b6a01ef667622b5bb5
Caitlin Bales <caitlin@bulubox.com>
2016-04-13 14:43:28 -0500

    Init file structure for node app

---
+++ server.js
@@ -0,0 +1,24 @@
+// set up
+var express = require('express');
+var cookieParser  = require('cookie-parser');
+var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
+var cors = require('cors');
+var instagram = require('./instagram');
+var app = express();
+
+app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.urlencoded({'extended': 'true'}));
+app.use(bodyParser.json());
+app.use(bodyParser.json({type: 'application/vnd.api+json'}));
+app.use(cors({origin: 'http://localhost:3000'}));
+app.use(cookieParser());
+
+app.use('/', instagram);
+
+app.get('*', function(req, res) {
+	res.sendfile('./public/index.html');
+});
+
+app.listen(3000);
+console.log("App listening on port 3000");
